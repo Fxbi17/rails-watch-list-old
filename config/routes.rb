84e1697888b51b48bc7773b9ecb7d65b96319d4f
Fabio Da Silva <fabio.dsv@hotmail.com>
2023-01-07 15:13:45 +0100

    corrections

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists, only: %i[new show create index] do
+  resources :lists, only: %i[new show create index destroy] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[destroy]
